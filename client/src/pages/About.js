749c5b24053050fa45998d457700f0181633b2f7
Skim1571 <skim1571@gmail.com>
2022-07-29 11:50:44 -0500

    css updates

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,13 +5,15 @@ const About = () => {
     <div>
       <div className="about">
         <h2 className="title">About</h2>
-        <h3>Developer: Steve Kim</h3>
-        <h4>This app is intended to allow you to create and choose a Golf Course's ScoreCard and then allows you to record your score.</h4>
-        <div>
-        <h5>Phase I: Golf Course Input</h5>
-        <h5>Phase II: Golf Course Import via API</h5>
-        <h5> Phase III: User Score Input</h5>
-        <h5>Phase IV: Multi-User Score Input</h5>
+        <div className="about-body">
+          <h3>Developer: Steve Kim</h3>
+          <h4>This app is intended to allow you to create and choose a Golf Course's ScoreCard and then allows you to record your score.</h4>
+          <div>
+          <h5>Phase I: Golf Course Input</h5>
+          <h5>Phase II: Golf Course Import via API</h5>
+          <h5> Phase III: User Score Input</h5>
+          <h5>Phase IV: Multi-User Score Input</h5>
+        </div>
         </div>
       </div>
     </div>
