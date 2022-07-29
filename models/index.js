14a95a26e1622ef00cc233535fb7f3189d22e019
Skim1571 <skim1571@gmail.com>
2022-07-28 22:13:14 -0500

    removed game schema as it is not in use yet

--- models/index.js
+++ models/index.js
@@ -1,17 +1,11 @@
 const { model } = require('mongoose');
-const GameSchema = require('./Game');
-const StrokeSchema = require('./Stroke');
 const CourseSchema = require('./Course')
 const ParSchema = require('./Par')
 
-const Game = model('Game', GameSchema);
-const Stroke = model('Stroke', StrokeSchema);
 const Course = model('Course', CourseSchema);
 const Par = model('Par', ParSchema);
 
 module.exports = {
-  Game,
-  Stroke,
   Course,
   Par
 };
