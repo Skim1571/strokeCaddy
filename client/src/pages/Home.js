50ee3a6a94f08fc3048dfa6dd967420f022bb03a
Skim1571 <skim1571@gmail.com>
2022-07-29 07:39:17 -0500

    css work: updated links to buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,34 @@
+import { useNavigate } from "react-router-dom"
 import { Link } from 'react-router-dom'
+const BASE_URL = 'http://localhost:3001'
 
 const Home = () => {
-  const BASE_URL = 'http://localhost:3001'
+
+
+  let navigate = useNavigate()
+  const createCourse = () => {
+    navigate(`/newcourse`)
+  }
+  const viewCourses = () => {
+    navigate(`/allcourses`)
+  }
 
   return (
-      <div className="home">
-        <h2 className="title">Stroke Caddy</h2>
-        <ul>
-          <li>
-          <Link to="newcourse">Create a New Course</Link>
-          </li>
-          <li>
-          <Link to="allcourses">View Courses</Link>
-          </li>
-        </ul>
+    <div className="home">
+      <h2 className="title">Stroke Caddy</h2>
+      <div className="home-grid">
+      <div className='card' onClick={() => createCourse()}>
+        <div className="info-wrapper">
+          <h3>Create a New Course</h3>
+        </div>
       </div>
+      <div className='card' onClick={() => viewCourses()}>
+        <div className="info-wrapper">
+          <h3>View Courses</h3>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
